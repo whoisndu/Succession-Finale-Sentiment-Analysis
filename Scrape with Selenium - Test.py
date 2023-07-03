add99810bc3c785554c33e0e2dacc7dbf294f8de
whoisndu <58919669+whoisndu@users.noreply.github.com>
2023-07-04 00:35:08 +0100

    Add files via upload

---
+++ Scrape with Selenium - Test.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+import csv
+
+def scrape_tweets():
+    hashtag1 = "#successionHBO"
+    hashtag2 = "successionfinale"
+    count = 0
+    max_tweets = 1000000
+    
+    # Configure Selenium with user agent of an older version of Chrome
+    options = Options()
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+    driver = webdriver.Chrome(options=options)
+    
+    # Scrape tweets with the specified hashtags until the maximum limit is reached
+    tweets = []
+    driver.get(f"https://twitter.com/search?q={hashtag1}%20OR%20{hashtag2}&src=typed_query")
+    
+    while count < max_tweets:
+        # Scroll down to load more tweets
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)  # Adjust the delay as needed
+        
+        # Find and extract tweet elements
+        tweet_elements = driver.find_elements(By.XPATH, '//div[@data-testid="tweet"]')
+        for tweet_element in tweet_elements:
+            count += 1
+            tweet_data = [
+                tweet_element.get_attribute('data-time'),  # Date and time
+                tweet_element.find_element(By.XPATH, './/a[starts-with(@href, "/")]').get_attribute('href'),  # URL
+                tweet_element.find_element(By.XPATH, './/div[@data-testid="tweet"]').text,  # Content
+                tweet_element.get_attribute('innerHTML'),  # Rendered Content
+                tweet_element.get_attribute('data-tweet-id'),  # ID
+                # ... Add more attributes as needed
+            ]
+            tweets.append(tweet_data)
+            print(f'Tweet #{count}: {tweet_data[2]}')
+            
+            if count >= max_tweets:
+                break
+        
+    # Save tweets to a CSV file
+    filename = r'C:\Users\Desktop\tweets.csv'
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            'Date', 'URL', 'Content', 'Rendered Content', 'ID', ...
+        ])
+        writer.writerows(tweets)
+        
+    driver.quit()
+    return count
+
+def main():
+    while True:
+        count = scrape_tweets()
+        print(f'Total tweets scraped: {count}')
+        
+        if count >= 1000000:
+            print('Maximum limit reached. Exiting the script.')
+            break
+        
+        time.sleep(600)  # Sleep for 10 minutes
+        
+        print('Sleeping for 3 minutes...')
+        time.sleep(180)  # Sleep for 3 minutes
+
+if __name__ == '__main__':
+    main()
+
---
+++ Scrape with snsscrape - Test.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import snscrape.modules.twitter as sntwitter
+import csv
+import time
+
+def scrape_tweets():
+    hashtag1 = "#successionHBO"
+    hashtag2 = "successionfinale"
+    count = 0
+    max_tweets = 1000000
+    
+    # Scrape tweets with the specified hashtags until the maximum limit is reached
+    tweets = []
+    for tweet in sntwitter.TwitterSearchScraper(f'{hashtag1} OR {hashtag2}').get_items():
+        count += 1
+        tweet_data = [
+            tweet.date, tweet.url, tweet.content, tweet.renderedContent, tweet.id,
+            tweet.user.username, tweet.user.displayname, tweet.user.id, tweet.user.description,
+            tweet.user.verified, tweet.user.created, tweet.user.followersCount, tweet.user.friendsCount,
+            tweet.user.statusesCount, tweet.user.location, tweet.user.descriptionUrls, tweet.user.linkUrl,
+            tweet.user.profileImageUrl, tweet.user.profileBannerUrl, tweet.replyCount, tweet.retweetCount,
+            tweet.likeCount, tweet.quoteCount, tweet.lang, tweet.source, tweet.retweetedTweet,
+            tweet.quotedTweet, tweet.mentionedUsers, tweet.hashtags
+        ]
+        tweets.append(tweet_data)
+        print(f'Tweet #{count}: {tweet.content}')
+        
+        if count >= max_tweets:
+            break
+        
+    # Save tweets to a CSV file
+    filename = r'C:\Users\NDU-PC\Desktop\tweets.csv'
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            'Date', 'URL', 'Content', 'Rendered Content', 'ID', 'Username', 'Display Name',
+            'User ID', 'User Description', 'Verified', 'User Created', 'Followers Count',
+            'Friends Count', 'Statuses Count', 'User Location', 'User Description URLs',
+            'User Link URL', 'User Profile Image URL', 'User Profile Banner URL', 'Reply Count',
+            'Retweet Count', 'Like Count', 'Quote Count', 'Language', 'Source', 'Retweeted Tweet',
+            'Quoted Tweet', 'Mentioned Users', 'Hashtags'
+        ])
+        writer.writerows(tweets)
+        
+    return count
+
+def main():
+    while True:
+        count = scrape_tweets()
+        print(f'Total tweets scraped: {count}')
+        
+        # Check if the maximum limit has been reached
+        if count >= 1000000:
+            print('Maximum limit reached. Exiting the script.')
+            break
+        
+        # Sleep for 10 minutes
+        time.sleep(600)
+        
+        # Sleep for 3 minutes
+        print('Sleeping for 3 minutes...')
+        time.sleep(180)
+
+if __name__ == '__main__':
+    main()
+
